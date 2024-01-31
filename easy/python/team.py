df2934ce87be62b341c4e460c0f21133ebb375eb
soandrade6 <sofiapl2018@gmail.com>
2024-01-30 21:44:16 -0500

    add python directory

---
+++ easy/python/team.py
@@ -0,0 +1,13 @@
+# https://codeforces.com/problemset/problem/231/A
+n = int(input())
+
+cont = 0
+for _ in range(n):
+    if sum(map(int, input().split())) >= 2:
+        cont += 1
+
+print(cont)
+
+
+    
+
---
+++ easy/python/word_capitalization.py
@@ -0,0 +1,3 @@
+#https://codeforces.com/problemset/problem/281/A
+word = input()
+print(word[0].capitalize() + word[1:])
